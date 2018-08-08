010914040e22ee42aa08483c915367de5e57f4e2
depaulobrunna <brunna@ifce.edu.br>
2018-08-08 15:41:07 -0300

    registrador ODR n muda

---
+++ led-test/Objects/led-test.d
@@ -0,0 +1,9 @@
+.\objects\led-test.o: led-test.c
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\Keil\STM32F0xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\Keil\STM32F0xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f051x8.h
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\core_cm0.h
+.\objects\led-test.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_version.h
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_compiler.h
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_armcc.h
+.\objects\led-test.o: C:\Keil_v5\ARM\PACK\Keil\STM32F0xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h
---
+++ led-test/led-test.c
@@ -0,0 +1,21 @@
+#include "stm32f0xx.h"                  // Device header
+
+
+int main()
+{
+#if 0
+	RCC->AHBENR |= _VAL2FLD(RCC_AHBENR_GPIOCEN, 1);
+	GPIOC->MODER |= _VAL2FLD(GPIO_MODER_MODER8, 1);
+	GPIOC->ODR &= ~GPIO_ODR_8;
+#endif
+	
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->MODER |= GPIO_MODER_MODER10_0|GPIO_MODER_MODER11_0;
+	GPIOB->ODR &= ~(GPIO_ODR_OD10);
+	
+	while(1)
+	{
+		//GPIOB->ODR &= ~(GPIO_ODR_10);
+	
+	}
+}
